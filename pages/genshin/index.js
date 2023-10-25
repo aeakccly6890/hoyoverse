4ea55e617eeeda813f412c14ae3cd5dd241db961
Aimer <aeakccly6890>
2023-10-25 21:02:45 +0800

    更新首页图片资源

--- pages/genshin/index.js
+++ pages/genshin/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Index = () => {
     return (
-        <div>
-            genshin impact
-        </div>
+        <>
+            <div className={"w-screen h-screen bg-cover bg-center bg-no-repeat bg-[url(/index/desk.jpg)]"}>
+            </div>
+        </>
     );
 };
 
